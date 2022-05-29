66956853de08785a353d9ecc9195d528795e2186
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2022-05-29 22:40:07 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+	name = 'haku-net-utils',
+	packages = ['hakunet'],
+	zip_safe = False,
+	version = '0.1.0',
+	description = 'A simple server framework',
+	author = 'KohakuBlueleaf',
+	author_email = 'apolloyeh0123@gmail.com',
+	keywords = ['Server', 'NetWork', 'p2p', 'asyncio'],
+)
